9ae4b74e0243428e2859eead6dff2787605455d1
Piotr Pryc <piotrekpryc@gmail.com>
2025-03-01 20:09:30 +0100

    Dodane chodzenie

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -10,36 +9,24 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction: -1, 0, 1
-	var direction = Input.get_axis("move_left", "move_right")
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
 	
 	# Flip the Sprite
-	if direction > 0:
+	if direction_x > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif direction_x < 0:
 		animated_sprite.flip_h = true
 	
 	# Play animations
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
+	if direction_x == 0 and direction_y == 0:
+		animated_sprite.play("idle")
 	else:
-		animated_sprite.play("jump")
-	
+		animated_sprite.play("run")
+
 	# Apply movement
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = direction_x * SPEED
+	velocity.y = direction_y * SPEED
 
 	move_and_slide()
