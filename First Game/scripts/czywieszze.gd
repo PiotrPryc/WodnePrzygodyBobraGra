e591c2040b5cb3c11668635d9329b53c8ec8944f
Piotr Pryc <piotrekpryc@gmail.com>
2025-03-02 15:26:04 +0100

    pytania

--- First Game/scripts/czywieszze.gd
+++ First Game/scripts/czywieszze.gd
@@ -4,23 +4,27 @@ extends Control
 @onready var close_button: Button = $Button
 @onready var pytanie_label: RichTextLabel = $RichTextLabel
 @onready var canvas_czywieszze = %CzyWieszZeCanvas
+@onready var hud = %Hud
 
 func _ustaw_pytanie() -> void:
-	var pytanie = ""
+	var ciekawostka_text = ""
 
 	match self.name:
 		"Wiedza1":
-			pytanie = "abc"
+			ciekawostka_text = "Najgłębsze jezioro na świecie, Bajkał, ma aż 1642 metry głębokości i zawiera około 20% niezamarzniętej słodkiej wody na Ziemi!"
 		"Wiedza2":
-			pytanie = "drugie"
+			ciekawostka_text = "Źródła artezyjskie to podziemne zbiorniki wody, które wypływają na powierzchnię dzięki naturalnemu ciśnieniu. W Polsce znajdziesz je m.in. w Nałęczowie i Ciechocinku."
 		"Wiedza3":
-			pytanie = "trzecie"
+			ciekawostka_text = "Lodowce, które kiedyś pokrywały Polskę, zostawiły po sobie jeziora polodowcowe, wzgórza morenowe i wielkie doliny. Przykładem jest Pojezierze Mazurskie!"
 		_:
-			pytanie = "Brak pytania"
+			ciekawostka_text = "Brak ciekawostki"
 
-	pytanie_label.text = pytanie
+	var ciekawostka_template = "[center][b]Czy wiesz, że...[/b]\n\n" + ciekawostka_text + "[/center]"
 
+	pytanie_label.text = ciekawostka_template
+			
 func _ready() -> void:
+	pytanie_label.bbcode_enabled = true
 	_ustaw_pytanie()
 	canvas_czywieszze.visible = false
 	visible = false
@@ -35,6 +39,8 @@ func show_popup() -> void:
 func _on_close_pressed() -> void:
 	canvas_czywieszze.visible = false
 	visible = false
+	hud.increase_score(1)
+	
 
 func toggle_visible() -> void:
 	canvas_czywieszze.visible = !canvas_czywieszze.visible
