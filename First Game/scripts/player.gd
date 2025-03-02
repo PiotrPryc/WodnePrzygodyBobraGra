98b6a2e3e77c3ee6e52bd6a5b63329413144cc73
Piotr Pryc <piotrekpryc@gmail.com>
2025-03-02 13:28:03 +0100

    dodana animacja bobra i zbieranie ksiazek

--- First Game/scripts/player.gd
+++ First Game/scripts/player.gd
@@ -8,18 +8,18 @@ func _physics_process(delta):
 	# Get the input direction: -1, 0, 1
 	var direction_x = Input.get_axis("move_left", "move_right")
 	var direction_y = Input.get_axis("move_up", "move_down")
-	
-	# Flip the Sprite
-	if direction_x > 0:
-		animated_sprite.flip_h = false
-	elif direction_x < 0:
-		animated_sprite.flip_h = true
-	
+		
 	# Play animations
 	if direction_x == 0 and direction_y == 0:
 		animated_sprite.play("idle")
-	else:
-		animated_sprite.play("run")
+	elif direction_x > 0:
+		animated_sprite.play("go_right")
+	elif direction_x < 0:
+		animated_sprite.play("go_left")
+	elif direction_y > 0:
+		animated_sprite.play("go_down")
+	elif direction_y < 0:
+		animated_sprite.play("go_up")
 
 	# Apply movement
 	velocity.x = direction_x * SPEED
