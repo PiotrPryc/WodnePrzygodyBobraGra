b4f935f5326a7e188fae39867cc1b1ea230cfc18
Piotr Pryc <piotrekpryc@gmail.com>
2025-03-02 14:13:22 +0100

    dodawanie_czasu

--- First Game/scripts/coin.gd
+++ First Game/scripts/coin.gd
@@ -2,7 +2,10 @@ extends Area2D
 
 @onready var game_manager = %GameManager
 @onready var animation_player = $AnimationPlayer
+@onready var hud = %Hud
 
 func _on_body_entered(body):
 	game_manager.add_point()
 	animation_player.play("pickup")
+	if hud:  
+		hud.add_time(30)
