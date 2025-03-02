1139292ff9f4bf3f2805965d7d3975bbd9bf98c0
Piotr Pryc <piotrekpryc@gmail.com>
2025-03-02 15:17:42 +0100

    dodane pytania

--- First Game/scripts/czywieszze.gd
+++ First Game/scripts/czywieszze.gd
@@ -2,10 +2,26 @@ extends Control
 
 
 @onready var close_button: Button = $Button
+@onready var pytanie_label: RichTextLabel = $RichTextLabel
 @onready var canvas_czywieszze = %CzyWieszZeCanvas
 
+func _ustaw_pytanie() -> void:
+	var pytanie = ""
+
+	match self.name:
+		"Wiedza1":
+			pytanie = "abc"
+		"Wiedza2":
+			pytanie = "drugie"
+		"Wiedza3":
+			pytanie = "trzecie"
+		_:
+			pytanie = "Brak pytania"
+
+	pytanie_label.text = pytanie
+
 func _ready() -> void:
-	# Hide the entire popup initially
+	_ustaw_pytanie()
 	canvas_czywieszze.visible = false
 	visible = false
 	close_button.pressed.connect(_on_close_pressed)
